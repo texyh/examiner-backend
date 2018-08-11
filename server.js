021043d6495da40efb05905e8de0ca3bf65bec29
texyh <onwuzulikee1@gmail.com>
2018-08-12 00:21:09 +0100

    added more packages and configured test

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+require('./config/config');
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('home');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`now listening on port ${process.env.PORT}`);
+})
---
+++ tests/server.test.js
@@ -0,0 +1,7 @@
+const expect = require('expect');
+
+describe('Server JS', () => {
+    it('should return true', () => {
+        expect(true).toBeTruthy();
+    })
+})

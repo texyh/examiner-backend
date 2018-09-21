29d71711de07e20ad5cf109c16cb35588190acfc
texyh <onwuzulikee1@gmail.com>
2018-09-21 21:44:40 +0100

    fixed Circle Ci

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,6 +1,6 @@
 const db = require('mongoose');
 const env = require('../config')
-console.log("=============================================" + env.MONGODB_URI + "==================================================")
+console.log("=============================================" + env.MONGODB_URI + ' ' +  process.env.NODE_ENV + "==================================================")
 db.Promise = global.Promise;
 
 db.connect(env.MONGODB_URI);
